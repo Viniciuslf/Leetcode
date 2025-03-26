3d32bbdbaa03e12212aba8076212315fca16ffba
Vinícius <63385115+Viniciuslf@users.noreply.github.com>
2025-03-26 20:55:09 -0300

    Update MiddleLinkedList.java

--- MiddleLinkedList.java
+++ MiddleLinkedList.java
@@ -1,7 +1,4 @@
-/*
- * Given the head of a singly linked list, return the middle node of the linked list. 
- * If there are two middle nodes, return the second middle node. 
- */
+
 public class MiddleLinkedList {
   public static void main(String[] args) {
     var head = new ListNode();
